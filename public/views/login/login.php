1c2bc4612c5101e9411bac0d9ee5a6053d3a0323
PedroHenriqueAzevedo <pedrcandidoenrique@gmail.com>
2025-04-15 18:55:26 -0300

    dev 3

--- public/views/login/login.php
+++ public/views/login/login.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../../../config/database.php'; // Conexão com o banco
 ?>
 
 <?php include '../cabecalho/header.php'; ?>
-<?php include '../cabecalho/tabela.php'; ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
