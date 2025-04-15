2c499c338131a7ee6ba88c6f1e57a1f5442b5008
PedroHenriqueAzevedo <pedrcandidoenrique@gmail.com>
2025-04-15 19:03:43 -0300

    dev 3

--- public/views/cabecalho/tabela_administrativa.php
+++ public/views/cabecalho/tabela_administrativa.php
@@ -9,9 +9,7 @@
                     <td class="p-2 flex-shrink-0"><a href="../cadastro/cadastro_jogador.php" class="btn btn-secondary btn-lg">Cadastro de Jogador</a></td>
                     <td class="p-2 flex-shrink-0"><a href="../login/login.php" class="btn btn-secondary btn-lg">Login</a></td>
                     <td class="p-2 flex-shrink-0"><a href="../avaliacao/visualizar_avaliacoes.php" class="btn btn-secondary btn-lg">Visualizar Avaliações</a></td>
-                    <td class="p-2 flex-shrink-0"><a href="../estatistica/cadastrar_estatistica_jogador.php" class="btn btn-secondary btn-lg">Cadastro Estatística Por Partida de Jogador</a></td>
                     <td class="p-2 flex-shrink-0"><a href="../estatistica/vizualizar_estatistica_jogador.php" class="btn btn-secondary btn-lg">Estatística do Jogador</a></td>
-                    <td class="p-2 flex-shrink-0"><a href="../substituicao/cadastrar_substituicao.php" class="btn btn-secondary btn-lg">Cadastrar Substituição</a></td>
                     <td class="p-2 flex-shrink-0"><a href="../substituicao/listar_substituicOES.php" class="btn btn-secondary btn-lg">Visualizar Substituição</a></td>
                     <td class="p-2 flex-shrink-0"><a href="../dashboard/dashboard_administrador.php" class="btn btn-secondary btn-lg">Pagina Inicial Do Admin</a></td> 
                 </tr>
