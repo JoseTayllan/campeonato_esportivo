791e80b5ca051847afdf1a50a33cb13befb31b83
JoseTayllan <josetayllan055@gmail.com>
2025-04-29 15:41:35 -0300

    ranking e estrutura

--- public/views/public/campeonato.php
+++ public/views/public/campeonato.php
@@ -3,7 +3,17 @@
 <div class="container mt-4">
     <h2><?= htmlspecialchars($dados['campeonato']['nome']) ?></h2>
 
-    <p class="text-muted mb-2">
+    <a href="/campeonato_esportivo/routes/ranking_campeonato.php?campeonato_id=<?= $dados['campeonato']['id'] ?>" 
+       class="btn btn-outline-primary btn-sm mb-3">
+       🥇 Ver Ranking do Campeonato
+    </a>
+    <a href="/campeonato_esportivo/public/views/campeonatos/visualizar_fases_rodadas.php?campeonato_id=<?= $dados['campeonato']['id'] ?>"
+   class="btn btn-outline-success btn-sm mb-3 ms-2">
+    📋 Ver Estrutura do Campeonato
+</a>
+
+
+    <p>
         Temporada: <?= htmlspecialchars($dados['campeonato']['temporada']) ?> |
         Formato: <?= htmlspecialchars($dados['campeonato']['formato']) ?> |
         Modalidade: <?= htmlspecialchars($dados['campeonato']['modalidade']) ?>
@@ -15,14 +25,7 @@
     <p style="text-align: justify;">
         <?= nl2br(htmlspecialchars($dados['campeonato']['descricao'] ?? 'Nenhuma descrição cadastrada ainda.')) ?>
     </p>
-    <a href="/campeonato_esportivo/public/views/campeonatos/visualizar_fases_rodadas.php?campeonato_id=<?= $dados['campeonato']['id'] ?>"
-   class="btn btn-primary btn-sm mt-3">
-    📋 Ver Estrutura do Campeonato
-</a>
 
-
-
-    <!-- Times Participantes -->
     <h4 class="mt-5">👥 Times Participantes</h4>
 
     <?php if (empty($dados['times'])): ?>
@@ -33,7 +36,7 @@
                 <div class="col text-center">
                     <div class="border rounded p-2 h-100 bg-light">
                         <img src="/campeonato_esportivo/public/<?= $time['escudo'] ?? 'img/perfil_padrao/perfil_padrao.png' ?>" 
-                             alt="Escudo <?= $time['nome'] ?>" width="60" height="60" class="mb-2">
+                             alt="Escudo <?= $time['nome'] ?>" width="60" class="mb-2">
                         <div class="fw-semibold"><?= htmlspecialchars($time['nome']) ?></div>
                         <?php if (!empty($time['codigo_publico'])): ?>
                             <a href="/campeonato_esportivo/public/views/time/perfil_time.php?codigo=<?= urlencode($time['codigo_publico']) ?>" 
